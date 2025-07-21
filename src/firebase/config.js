17a17c7a34e0eb1e2e8d0a49cdf1de5708ebe142
Imran Jaleel <imranjaleel2331@gmail.com>
2025-07-21 13:57:47 +0530

    hide the firebase API keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7djxAWqSFmKX7BVj4YsskKTuY6HJ2_eQ",
-  authDomain: "book-club-12d3d.firebaseapp.com",
-  projectId: "book-club-12d3d",
-  storageBucket: "book-club-12d3d.firebasestorage.app",
-  messagingSenderId: "380144287169",
-  appId: "1:380144287169:web:d2a8556afb9a9775e48340",
-  measurementId: "G-BPGMGQ0NKX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
